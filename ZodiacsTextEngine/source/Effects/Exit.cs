0bcad5100a5e7fbdf5dae1d9bd34a45737f87e82
D3TONAT0R <gdv@swissonline.ch>
2025-08-07 16:07:48 +0200

    Exit effect

---
+++ ZodiacsTextEngine/source/Effects/Exit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ZodiacsTextEngine.Effects
+{
+	public class Exit : Effect
+	{
+		public override Task Execute(EffectGroup g)
+		{
+			Environment.Exit(0);
+			return Task.CompletedTask;
+		}
+	}
+}
